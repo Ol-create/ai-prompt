eb2c28180077ee50059ccbf80fb6c145721809c4
Oluyemi Paul <oluola96@gmail.com>
2024-12-02 18:20:10 +0100

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
     const { data: session } = useSession()
     const [providers, setProviders ] = useState(null);
     const [ toggleDropdown, setToggleDropdown ] = useState(false)
-
+    {alert(providers)}
     useEffect(() => {
         const setUpProviders = async () => {
             const response = await getProviders();
